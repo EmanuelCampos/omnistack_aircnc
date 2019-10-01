d8b03efedc30685c593489d93b0ad9d23f237ada
Arli <arli.machado@gmail.com>
2019-10-01 17:24:09 -0300

    Efetuando reserva

--- BACKEND/src/routes.js
+++ BACKEND/src/routes.js
@@ -4,6 +4,8 @@ const uploadConfig = require("./config/upload");
 
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
+const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -12,5 +14,8 @@ routes.post("/sessions", SessionController.store);
 
 routes.get("/spots", SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+routes.post("/spots/:spot_id/bookings", BookingController.store);
+
+routes.get("/dashboard", DashboardController.show);
 
 module.exports = routes;
