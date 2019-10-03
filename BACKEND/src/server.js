da9134d35eb3e1fe16fb7af2d24f25f231521f4f
Arli <arli.machado@gmail.com>
2019-10-03 17:07:01 -0300

    Exibindo a url da image no serviço de spots

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const path = require("path");
 
 const cors = require("cors");
 
@@ -16,6 +17,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
+app.use("/files/", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
