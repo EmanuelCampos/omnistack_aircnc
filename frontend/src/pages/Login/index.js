998de5815ab09171b097956dc42e5dcdb7492575
Arli <arli.machado@gmail.com>
2019-10-03 17:46:03 -0300

    Componentizando as paginas de login, dashboard e new

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+import api from '../../services/api';
+
+// import { Container } from './styles';
+
+export default function Login({ history }) {
+
+  const [email, setEmail] = useState('')
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    const response = await api.post('/sessions', { email });
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+
+    history.push('/dashboard');
+    
+  }
+
+  return (
+    
+    <>
+      <p>
+          Ofereça <strong>spots para programadores e encontre 
+              <strong>talentos</strong> para sua empresa</strong>
+        </p>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+    </>
+  );
+}
