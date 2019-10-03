5d905fecb4c569145dbc35eeae9c0e7528fb562c
Arli <arli.machado@gmail.com>
2019-10-03 14:55:41 -0300

    Definindo layout da tela de login

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+      <div className="content">
+        <p>
+          Ofereça <strong>spots para programadores e encontre 
+              <strong>talentos</strong> para sua empresa</strong>
+        </p>
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor e-mail"
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
